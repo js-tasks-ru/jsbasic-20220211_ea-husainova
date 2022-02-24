8d0644e431220227f1a5bba8f8c75b23048871a7
Elvira Khusainova <ea.husainova@yandex.ru>
2022-02-24 20:03:12 +0500

    Решение для camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,15 @@
+function ucFirst(str) {
+  if (!str) {
+    return str;
+  }
+  return str[0].toUpperCase() + str.substring(1);
+}
+
 function camelize(str) {
-  // ваш код...
+  /*let tokens = str.split('-');
+  for (let i = 1; i < tokens.length; i++) {
+    tokens[i] = ucFirst(tokens[i]);
+  }
+  return tokens.join('');*/
+  return str.split('-').map((token, idx) => idx === 0 ? token : ucFirst(token)).join('');
 }
