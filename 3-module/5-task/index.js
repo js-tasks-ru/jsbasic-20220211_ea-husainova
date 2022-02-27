5d8250a69a62ac184e39bf0b2c4e43abfb8e7fe2
Elvira Khusainova <ea.husainova@yandex.ru>
2022-02-27 17:27:28 +0500

    Min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
-  // ваш код...
+  let nums = str.split(' ').map(item => +item).filter(item => !isNaN(item));
+  let min = nums[0];
+  let max = nums[0];
+  for (let i = 0; i < nums.length; i += 1) {
+    if (nums[i] < min) {
+      min = nums[i];
+    } else if (nums[i] > max) {
+      max = nums[i];
+    }
+  }
+  return {min, max}
 }
