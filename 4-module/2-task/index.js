6715196e3e1f3cc6f20e3c06e4a7d3ba64fc97ce
Elvira Khusainova <ea.husainova@yandex.ru>
2022-03-05 17:11:50 +0500

    Выделение ячеек по диагонали

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,6 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let rowsCol = table.rows;
+  for (let i = 0; i < rowsCol.length; i++) {
+    rowsCol[i].cells[i].style.backgroundColor = 'red';
+  }
 }
