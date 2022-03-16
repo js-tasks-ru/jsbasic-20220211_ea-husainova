931e2ed220a7c7897aa44b680b86f3f85b49acf3
Elvira Khusainova <ea.husainova@yandex.ru>
2022-03-16 19:46:29 +0500

    Таблица с удаляемыми строками

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,38 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = this.#getTable(rows);
+    this.elem.querySelectorAll('.close-button').forEach(closeButton =>
+      closeButton.addEventListener('click', function() {
+        closeButton.closest('tr').remove();
+    }));
+  }
+  #getTable(rows) {
+    let res = document.createElement('table');
+    res.innerHTML = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        ${this.#getRows(rows)}
+      </tbody>`;
+    return res;
+  }
+  #getRows(rows) {
+    return rows.map(row => `
+      <tr>
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button class="close-button">X</button></td>
+      </tr>
+      `).join('');
   }
 }
